a963f0de7e1ffb058181deb1855788f0b72e4662
ngocthieuff <ptnngoc22@gmail.com>
2022-09-10 15:48:10 +0700

    add UI ethereum card

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,4 +1,6 @@
 import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -44,7 +46,26 @@ const Welcome = () => {
                         </div>
                     </div>
 
-
+                    <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                        <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                <SiEthereum fontSize={21} color="#fff" />
+                                </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
+                            </div>
+                            <div>
+                                <p className="text-white font-light text-sm">
+                                0xNgocccccccccc
+                                </p>
+                                <p className="text-white font-semibold text-lg mt-1">
+                                Ethereum
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
