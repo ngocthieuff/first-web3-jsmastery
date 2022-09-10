97215bc3555738e37665cbce44fa6ee0ea07345a
ngocthieuff <ptnngoc22@gmail.com>
2022-09-10 16:13:03 +0700

    add loader ui

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -90,7 +90,7 @@ const Welcome = () => {
 
                             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-                            {false
+                            {true
                             ? <Loader />
                             : (
                                 <button
